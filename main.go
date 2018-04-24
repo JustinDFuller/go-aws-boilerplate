a4c79e5024d8d843dc94d5cdcb20e76c36d5d00d
Justin Fuller <justindanielfuller@gmail.com>
2018-04-24 14:01:12 -0400

    Update boilerplate to reflect correct way of interfacing with API Gateway requests.

--- main.go
+++ main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-  "encoding/json"
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type HeaderResponse struct {
-  AccessControlAllowOrigin string `json:"Access-Control-Allow-Origin"`
-  AccessControlAllowCredentials bool `json:"Access-Control-Allow-Credentials"`
-  ContentType string `json:"Content-Type"`
-}
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	stringResponse, _ := json.Marshal("Hello world")
 
-type Response struct {
-  IsBase64Encoded bool `json:"isBase64Encoded"`
-  StatusCode int `json:"statusCode"`
-  Headers HeaderResponse `json:"headers"`
-  Body string `json:"body"`
-}
-
-var response = Response{
-  IsBase64Encoded: false,
-  StatusCode: 200,
-  Headers: HeaderResponse{
-    AccessControlAllowOrigin : "*",
-    AccessControlAllowCredentials : true,
-    ContentType: "application/json",
-  },
-};
-
-func HandleRequest() (Response, error) {
-  stringifiedBody, _ := json.Marshal("Hello world")
-  response.Body = string(stringifiedBody)
-  return response, nil
+	return events.APIGatewayProxyResponse{
+		Body:       string(stringResponse),
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+	}, nil
 }
 
 func main() {
